3eb30c31bbbe58b9609d207589cb1698ffe1e0eb
katerinahristjuk <katerina.hristjuk@gmail.com>
2021-08-14 00:38:02 +0200

    api-users comment

--- Aclient/src/api/users.js
+++ Aclient/src/api/users.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const URL = 'http://localhost:7000/service-heating/api/v1';
 
-export const getUsers = () => axios.get(`${URL}/users`);
+// export const getUsers = () => axios.get(`${URL}/users`);
 export const register = (newUser) => axios.post(`${URL}/register`, newUser);
 export const login = (user) => axios.post(`${URL}/login`, user);
-export const updateUser = (id, user) => axios.patch(`${URL}/${id}`, user);
-export const deleteUser = (id) => axios.delete(`${URL}/${id}`);
+// export const updateUser = (id, user) => axios.patch(`${URL}/${id}`, user);
+// export const deleteUser = (id) => axios.delete(`${URL}/${id}`);
